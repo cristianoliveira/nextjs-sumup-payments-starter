44b1a35ea47b8a8da10488992bb1ad71c38236fa
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-02-24 19:25:46 +0100

    fix: remove unused parameter

--- modules/app-api-client.ts
+++ modules/app-api-client.ts
@@ -1,12 +1,11 @@
 export default {
-  createCheckout: ({ amount, paymentType: payment_type }) =>
+  createCheckout: ({ paymentType: payment_type }) =>
     fetch('/api/create-checkout', {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
       },
       body: JSON.stringify({
-        amount,
         payment_type,
       }),
     }).then((r) => r.json()),
