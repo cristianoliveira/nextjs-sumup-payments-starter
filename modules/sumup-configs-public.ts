4c2ae8e3faa889a79b0a1fb33f237557ae312fa1
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-02-28 11:12:16 +0100

    docs: include links to where find the configs

--- modules/sumup-configs-public.ts
+++ modules/sumup-configs-public.ts
@@ -1,5 +1,7 @@
 export default {
   swift_checkout_sdk: process.env.NEXT_PUBLIC_SUMUP_SWIFT_CHECKOUT_SDK,
   payment_widget_sdK: process.env.NEXT_PUBLIC_SUMUP_PAYMENT_WIDGET_SDK,
+  // To be able to generate this see:
+  // see https://js.sumup.com/showroom
   merchant_public_key: process.env.NEXT_PUBLIC_SUMUP_PUBLIC_MERCHANT_KEY,
 };
--- modules/sumup-configs.ts
+++ modules/sumup-configs.ts
@@ -2,10 +2,12 @@ export default {
   api_url: process.env.SUMUP_API_URL,
 
   // Merchant Information
+  // Check: https://me.sumup.com
   merchant_code: process.env.SUMUP_MERCHANT_CODE,
   merchant_email: process.env.SUMUP_MERCHANT_EMAIL,
 
   // SumUp Api Credentials
+  // Check https://developer.sumup.com/protected/oauth-apps/
   client_id: process.env.SUMUP_API_CLIENT_ID,
   client_secret: process.env.SUMUP_API_CLIENT_SECRET,
 
