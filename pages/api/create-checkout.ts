ad217deb370b90c428dad9b88942bdf1400c32c7
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-03-10 22:31:45 +0100

    feat: handle internal http errors

--- pages/api/create-checkout.ts
+++ pages/api/create-checkout.ts
@@ -8,25 +8,38 @@ const api = apiInit({ apiUrl: configs.api_url });
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { payment_type } = req.body;
   const { client_id, client_secret } = configs;
-  const { access_token } = await api.auth.fetchAccessToken({
-    client_id,
-    client_secret,
-    grant_type: 'client_credentials',
-    scope: 'payments',
-  });
+  const token = await api.auth
+    .fetchAccessToken({
+      client_id,
+      client_secret,
+      grant_type: 'client_credentials',
+      scope: 'payments',
+    })
+    .catch((err) => {
+      res.status(err.code || 500).json({ ...err });
+      return null;
+    });
 
-  const response = await api.checkouts.createCheckout({
-    access_token,
-    payload: {
-      checkout_reference: `checkout-ref-${Math.random()}`,
-      merchant_code: configs.merchant_code,
-      return_url: `https://${process.env.VERCEL_URL}/thanks`,
-      redirect_url: `https://${process.env.VERCEL_URL}/thanks`,
-      amount: configs.donation_amount,
-      payment_type,
-      currency: configs.currency,
-    },
-  });
+  if (!token) {
+    return;
+  }
 
-  res.status(200).json(response);
+  try {
+    const response = await api.checkouts.createCheckout({
+      access_token: token.access_token,
+      payload: {
+        checkout_reference: `checkout-ref-${Math.random()}`,
+        merchant_code: configs.merchant_code,
+        return_url: `https://${process.env.VERCEL_URL}/thanks`,
+        redirect_url: `https://${process.env.VERCEL_URL}/thanks`,
+        amount: configs.donation_amount,
+        payment_type,
+        currency: configs.currency,
+      },
+    });
+
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(err.code || 500).json({ ...err });
+  }
 };
